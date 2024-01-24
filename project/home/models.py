ef13528fe6d2a67ac5a6bfe22d7828cea15baaf0
Abhishek16Verma <Abhishek.verma@nmgtechnologies.com>
2024-01-24 20:11:42 +0530

    add models

--- project/home/models.py
+++ project/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    id = models.AutoField()
+    name = models.CharField(max_length=100)
+    food = models.CharField(max_length=100)
+
+class Profile(models.Model):
+    id = models.AutoField()
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
